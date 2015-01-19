592774c4dc1ab4222613696a2743637770497843
Spencer Alger <spencer@spenceralger.com>
2015-01-18 18:52:38 -0700

    move webpack config to gulp

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var _ = require('lodash');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var resolve = require('path').resolve;
+var abs = resolve.bind(null, __dirname);
+
+/**
+ * path config
+ * all paths should be absolute
+ *
+ * @type {object}
+ */
+var paths = {};
+
+// the projects root directory
+paths.ROOT = abs('.');
+
+// the source files for your app
+paths.SRC = abs('src');
+
+// the dir used to create archives in DIST
+paths.DIST = abs('dist');
+
+// dir for final tar/zip version of the site
+paths.BUILD = abs('build');
+
+// main module, imports all other modules for your app
+paths.ENTRY = resolve(paths.SRC, 'index.js');
+
+// output file for the bundle
+paths.BUNDLE = resolve(paths.BUILD, 'bundle.js');
+
+/**
+ * Start a webpack-dev-server that will compile
+ * our files and serve them
+ *
+ * info: http://webpack.github.io/docs/webpack-dev-server.html
+ * config docs: http://webpack.github.io/docs/configuration.html
+ */
+gulp.task('server', function (done) {
+  var express = require('express');
+  var webpack = require('webpack');
+
+  var app = express();
+  app.use(express.static(paths.SRC));
+  app.use(express.static(paths.BUILD));
+
+  getPacker(true).watch(200, function (err, stats) {
+    if (err)
+      return gutil.error(new gutil.PluginError('webpack', err));
+
+    if (stats.hasErrors() || stats.hasWarnings())
+      return gutil.log(stats.toString());
+
+    var names = stats.toJson().assets.map(function (asset) {
+      return asset.name;
+    });
+
+    gutil.log(gutil.colors.green('✓'), 're-built', names.join(','));
+  });
+
+  app.listen(8000, function (err) {
+    if (err) done(new gutil.PluginError('webpack', err));
+    else gutil.log('Server listening at http://localhost:8080');
+  });
+});
+
+function getPacker(dev) {
+  return webpack(require('./webpack.build.config.js')(paths, dev));
+}
---
+++ webpack.build.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var dirname = require('path').dirname;
+var basename = require('path').basename;
+
+module.exports = function (paths, dev) {
+  return {
+    devtool: dev ? 'source-map' : null,
+    context: paths.SRC,
+    entry: paths.ENTRY,
+    output: {
+      path: dirname(paths.BUNDLE),
+      filename: basename(paths.BUNDLE),
+      devtoolModuleFilenameTemplate: '[resource-path]',
+    },
+    module: {
+      loaders: [
+        // es6 modules
+        {
+          test: /\.js$/,
+          exclude: /node_modules|bower_components/,
+          loader: '6to5-loader'
+        },
+
+        // less styles
+        {
+          test: /\.less$/,
+          loader: 'style-loader!css-loader!less-loader'
+        }
+      ]
+    },
+
+    // extend the lookup logic to simplify import ids
+    //
+    // allows `import 'style/main.less'` from anywhere, imports `src/style/main.less`
+    resolve: {
+      extensions: ['', '.js', '.json'],
+      modulesDirectories: ['bower_components', 'node_modules'],
+      root: [ paths.SRC ]
+    },
+
+    // utilize bower.json/main setting for bower_components
+    plugins: [
+      new webpack.ResolverPlugin(
+        new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+      )
+    ]
+  };
+};
